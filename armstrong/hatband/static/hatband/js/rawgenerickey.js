4ce9b3f913d8721b82518a430b6f577cd18957bd
Dave McLain <dmclain@texastribune.org>
2012-05-23 12:51:30 -0500

    semicolons for everyone

--- armstrong/hatband/static/hatband/js/rawgenerickey.js
+++ armstrong/hatband/static/hatband/js/rawgenerickey.js
@@ -7,5 +7,5 @@ armstrong.widgets.raw_generic_key = function(type_input, id_input, picker_anchor
 	type_input.change(function(){
 		var ct = armstrong.widgets.raw_generic_key.content_types[type_input.val()];
 		picker_anchor.attr('href', '/admin/' + ct.app_label + '/' + ct.model);
-	})
+	});
 };
